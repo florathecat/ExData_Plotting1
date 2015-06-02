02abab9a3ff2892801683eadd7b1ca4d1054c653
florathecat <wumeow@gmail.com>
2015-06-01 23:10:24 -0400

    plot3_files
    plot3.R and plot3.png

--- plot3.R
+++ plot3.R
@@ -13,16 +13,18 @@ dates <- as.POSIXct(paste(hpc2$Date, hpc2$Time, sep=" "), tz="EST", "%d/%m/%Y %H
 
 #open a png device to draw and save plot in png format with indicated specifications
 png("plot3.png", width=480, height=480, units="px", res=120)
+par(cex.lab=0.8, cex.axis=0.8)
 
 #draw plot 3, first without tickmarks and labeling at x axis
-plot3 <- plot(dates, hpc2$Sub_metering_1, type="l", ylab = "Energy Sub metering")
-#somehow because I had set the x axis in plot 2 I don't have to set it in plot 3 now.
-lines(dates, hpc2$Sub_metering_2, type="l", col="red")
-lines(dates, hpc2$Sub_metering_3, type="l", col="blue")
-legend("topright", names(hpc2)[7:9], lty=1, col=c("black", "red", "blue"), cex=0.8)
+plot3 <- plot(dates, hpc2$Sub_metering_1, type="l", ylab = "Energy sub metering", xaxt="n", xlab=NA)
 
-#put markings on x axis at the beginning of the two days, and the end of the 2nd day
+#somehow because I had set the x axis in plot 2 I actually don't have to set it again in plot 3.
+#but I will set it again (xaxt="n", xlab=NA, put markings on x axis, etc.)
 axis(1, c("Thu", "Fri", "Sat"), at= c(min(dates), dates[which(dates== "2007-02-02 00:00:00 EST")],
                                       max(dates)+1))
+
+lines(dates, hpc2$Sub_metering_2, type="l", col="red")
+lines(dates, hpc2$Sub_metering_3, type="l", col="blue")
+legend("topright", names(hpc2)[7:9], lty=1, col=c("black", "red", "blue"), cex=0.7)
 
 dev.off()
